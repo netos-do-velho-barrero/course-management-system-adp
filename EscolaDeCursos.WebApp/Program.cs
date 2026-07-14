82be13713661c9f7024246927b828ae11ad158e8
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-07-14 00:00:10 -0300

    chore: set up UptimeRobot monitoring

--- EscolaDeCursos.WebApp/Program.cs
+++ EscolaDeCursos.WebApp/Program.cs
@@ -1,6 +1,8 @@
 using EscolaDeCursos.Aplicacao;
 using EscolaDeCursos.Infra;
+using EscolaDeCursos.Infra.Compartilhado.Orm;
 using EscolaDeCursos.WebApp.Compartilhado;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,12 +11,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddInfraRepositories(builder.Configuration, builder.Logging);
 builder.Services.AddApplicationServices();
 builder.Services.AddPresentationConfig(builder.Configuration);
+builder.Services.AddHealthChecks()
+    .AddDbContextCheck<EscolaDeCursosDbContext>(
+        name: "database_check",
+        failureStatus: HealthStatus.Unhealthy,
+        tags: ["ready"]
+    );
 
 var app = builder.Build();
 
 // Middlewares de roteamento
 app.UseRouting();
 app.MapDefaultControllerRoute();
+
+app.MapHealthChecks("/health");
 
 app.Run();
 // Execução do Servidor
